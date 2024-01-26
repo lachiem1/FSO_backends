2bf1f62b719bf301e900d54f78149a1523d5545c
lachiem1 <lachiem1@gmail.com>
2024-01-26 16:22:19 +1100

    connect backend with frontend

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.static('dist'));
+app.use(cors());
 
 let notes = [
   {
@@ -47,7 +51,7 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id);
 
   response.status(204).end();
-})
+});
 
 app.post('/api/notes', (request, response) => {
   const body = request.body;
@@ -67,16 +71,15 @@ app.post('/api/notes', (request, response) => {
   notes = notes.concat(note);
   
   response.json(note);
-})
+});
 
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
 
   return maxId;
-}
+};
 
-
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
