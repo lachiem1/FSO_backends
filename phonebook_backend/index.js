603524a904bfae9ce7683656dd6c0522af255bd5
lachiem1 <lachiem1@gmail.com>
2024-01-26 16:19:18 +1100

    fin 3.9

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 let phonebookData = [
     { 
@@ -100,10 +102,10 @@ app.post('/api/persons', (request, response) => {
     };
 
     phonebookData = phonebookData.concat(newPerson);
-
+    
     response.json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
