09b749d65bb3e9c7d4718313f75a4008eeb2f71c
lachiem1 <lachiem1@gmail.com>
2024-01-28 16:44:01 +1100

    get and post to mongodb

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -10,23 +10,6 @@ require('dotenv').config(); // has to be imported before the model so that env v
 
 const Note = require('./models/note')
 
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     important: true
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only JavaScript",
-//     important: false
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     important: true
-//   }
-// ];
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>');
@@ -39,17 +22,9 @@ app.get('/api/notes', (request, response) => {
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find(note => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note);
-  }
-  else {
-    response.status(404).end();
-  }
-
-  response.json(note);
+  });
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -68,22 +43,15 @@ app.post('/api/notes', (request, response) => {
     })
   }
   
-  const note = {
+  const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-    id: generateId()
-  };
+  });
 
-  notes = notes.concat(note);
-  
-  response.json(note);
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  });
 });
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
-
-  return maxId;
-};
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
