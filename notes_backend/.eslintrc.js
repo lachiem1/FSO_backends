60c643e0173f05712f6129560a011d34ec2d72fe
lachiem1 <lachiem1@gmail.com>
2024-01-30 17:30:28 +1100

    added linter

---
+++ notes_backend/.eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+	'env': {
+		'node': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'overrides': [
+		{
+			'env': {
+				'node': true
+			},
+			'files': [
+				'.eslintrc.{js,cjs}'
+			],
+			'parserOptions': {
+				'sourceType': 'script'
+			}
+		}
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [
+			'error', 'always'
+		],
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true }
+		],
+		'no-console': 0
+	}
+}
