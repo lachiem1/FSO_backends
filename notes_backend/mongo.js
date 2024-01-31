7d2033adff424891087ca09af226165b7fca0207
lachiem1 <lachiem1@gmail.com>
2024-01-31 13:58:27 +1100

    change style enforcer from eslint to prettier

--- notes_backend/mongo.js
+++ notes_backend/mongo.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
+    console.log("give password as argument");
     process.exit(1);
 }
 
@@ -9,7 +9,7 @@ const password = process.argv[2];
 
 const url = `mongodb+srv://lachiem1:${password}@mongodb-learning.tvusz9q.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
@@ -17,21 +17,21 @@ const noteSchema = new mongoose.Schema({
     important: Boolean,
 });
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-    content: 'HTML is easy',
+    content: "HTML is easy",
     important: true,
 });
 
-note.save().then(result => {
-    console.log('note saved!')
+note.save().then((result) => {
+    console.log("note saved!");
     // mongoose.connection.close()
 });
 
-Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
+Note.find({}).then((result) => {
+    result.forEach((note) => {
+        console.log(note);
     });
     mongoose.connection.close();
 });
